b8a8ce22ad265b1de81eb93f128bf6cba703f892
chittaranjan <chittaranjan.swain@oracle.com>
2018-10-05 22:36:43 +0530

    Fixed issue in gradle build to read file from command line

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,9 +1,36 @@
-import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Solution {
 
-    private List<Ballot> ballots;
     public static void main(String... args) {
-        System.out.println("Solution...");
+        System.out.println(args[1]);
+        Scanner input = null;
+        if (0 < args.length) {
+            try {
+                input = new Scanner(new File(args[1]));
+            } catch (FileNotFoundException ex) {
+                System.out.println("No File Found!");
+                return;
+            }
+
+        } else {
+            System.err.println("Invalid arguments count:" + args.length);
+            System.exit(0);
+        }
+
+        while(input.hasNext()){
+            System.out.println(input.nextLine());
+        }
+
+
+        Scanner in = new Scanner(System.in);
+        String line = null;
+        while (line == null || !line.equals("tally")) {
+
+            line = in.nextLine();
+            System.out.println(line);
+        }
     }
 }
